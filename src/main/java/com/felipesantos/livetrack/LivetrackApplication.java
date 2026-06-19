96ac983beb0b0573b89aedd0bd1c32a67e7687a1
Felipe Santos <fs518657@gmail.com>
2026-06-19 12:31:42 -0300

    chore: Adiciona arquivos de configuração do Maven e estrutura inicial do projeto Livetrack

---
+++ src/main/java/com/felipesantos/livetrack/LivetrackApplication.java
@@ -0,0 +1,13 @@
+package com.felipesantos.livetrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivetrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivetrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipesantos/livetrack/LivetrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipesantos.livetrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivetrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
